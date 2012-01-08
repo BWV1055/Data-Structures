a11d9591cb709a78e386e74d07b4f4be081b1476
BWV1055 <zarzu.razvan@gmail.com>
2012-01-09 00:09:02 +0200

    Linked list initial commit

--- bds/linked_list.c
+++ bds/linked_list.c
@@ -1,35 +1,145 @@
 /*
- *
+ *	Singly linked list operation implementation
  *
  */
 
 #include "linked_list.h"
 
-void insert_node(struct linked_list list, struct generic_data d, uint pos)
+void ll_insert(struct linked_list *l, struct generic_data d, int pos)
 {
 	if(pos<=0)
-	{ /*error*/ }
-	if(pos>list.total+1)
-	{ /*error*/ }
+		mds_error(1, "Cannot insert element at negative index");
+	if(pos>l->size+1)
+		mds_error(1, "Insert new element with out of range index");
 
-	node *to_insert = (node*)malloc(sizeof(node));
-	node *prev = (node*)malloc(sizeof(node));
-	to_insert->data = d;
-
-	to_insert->next = list.head;
-	prev = list.head;
-	while(pos--)
-	{
-		prev = to_insert->next;
-		to_insert->next = to_insert->next->next;
+	struct node *ins = malloc(sizeof *ins);
+	ins->next = l->head;
+	if(pos==1)
+		l->head = ins;
+	else {
+		struct node *prev;
+		//we are interested in prev, which should be one position behind pos
+		pos--;
+		while(pos--) {
+			prev = ins->next;
+			ins->next = ins->next->next;
+		}
+		prev->next = ins;
 	}
-	prev = to_insert;
-}
-	
-struct linked_list* ll_init() {
-	struct linked_list* new = malloc(sizeof(*new));
-	new->total = 0;
-	new->head = NULL;
-	return new;
-}	
+	ins->data = d;
+	l->size++;
+}		
 
+int ll_insert_after(struct linked_list *l, struct generic_data d, struct generic_data s)
+{
+	if(l->size==0)
+		return -1;
+	
+	/* pos is the index of the element pointed to by it */
+	int pos = 0;
+	struct node *it = l->head, *ins = malloc(sizeof *ins);
+	while(it && !EQUAL(it->data, d)) {
+		it = it->next;
+		pos++;
+	}
+	/* it is pointing to the NULL at the end:
+	 * the last element index is l->size-1 */
+	if(pos==l->size)
+		return -1;
+
+	ins->next = it->next;
+	ins->data = s;
+	it->next = ins;
+	l->size++;
+	return pos+1;
+}	
+	
+struct node* ll_recursive(struct node *it, struct generic_data d, struct generic_data s)
+{
+	struct node *next;	
+	if(!EQUAL(it->data, d)) {
+		if(it->next) {
+			if(next=ll_recursive(it->next, d, s)) {
+				it->next = next;
+				return it;
+			} else
+				return NULL;
+		} else 
+			return NULL;
+	} else {
+		struct node *new = malloc(sizeof *new);
+		new->data = s;
+		new->next = it;
+		return new;
+	}
+}		
+
+int ll_insert_before(struct linked_list *l, struct generic_data d, struct generic_data s)
+{
+	struct node *next;
+	if(!l->size)
+		return -1;
+
+	if(next=ll_recursive(l->head, d, s)) {
+		l->head = next;
+		l->size++;
+		return 1;
+	} else
+		return -1;
+}
+
+struct generic_data ll_remove(struct linked_list *l, int pos)
+{
+	if(ll_isEmpty(l))
+		mds_error(1, "Linked list is empty");
+	if(pos<=0)
+		mds_error(1, "Cannot remove element with negative index");
+	if(pos>l->size)
+		mds_error(1, "Remove new element with out of range index");
+	
+	struct node *rm = l->head;
+	struct generic_data data;
+	if(pos==1) {
+		l->head = rm->next;
+	} else {
+		struct node *prev;
+		while(pos--) {
+			prev = rm;
+			rm = rm->next;
+		}
+		prev->next = rm->next;
+	}
+	data = rm->data;
+	free(rm);
+	l->size--;
+	return data;
+}
+
+int ll_size(struct linked_list *l)
+{
+	return l->size;
+}
+
+int ll_isEmpty(struct linked_list *l)
+{
+	return (ll_size(l)==0);
+}
+
+struct linked_list* ll_init()
+{
+	struct linked_list *l = malloc(sizeof *l);	
+	l->head = NULL;
+	l->size = 0;
+	return l;
+}
+
+void ll_walk(struct linked_list *l, void (*f)(struct generic_data))
+{
+	if(l->size>0) {
+		struct node *it = l->head;
+		while(it!=NULL) {
+			f(it->data);
+			it = it->next;
+		}
+	}
+}
--- bds/linked_list.h
+++ bds/linked_list.h
@@ -1,7 +1,33 @@
 /*
  *	Linked list header file
  *
+ * Linked list definition and basic operations declaration
+ * The list of methods defined here is used in all linked list data structures,
+ * with possible additions in more sophisticated linked lists types
+ * This is the basic singly linked list, it stores a pointer in the head field
+ *
+ * The interface works with data, NOT with nodes, so that someone cannot tamper
+ * the list (by accessing the next field, for example). This has some consequences
+ * on the operations available on the list:
+ * - the insert and remove functions cannot specify a node, only an index to insert
+ *   after (to insert before, just decrease the index by one), or a value. 
+ * - if a value is specified, the new data will be inserted after or before 
+ *	 the FIRST occurence of the value specified 
+ * - in the future, there will be functions to insert/remove before/after the last
+ *   occurence of the specified value, or before/after the nth occurence
+ *
+ * There is a double guard against out of bounds elements:
+ * 1) a size field equal to the number of elements in the list
+ * 2) the next field of the last elements points to NULL
+ *
+ * Errors: 
+ *	For out of range indices, a message is printed to stderr (or a file)
+ *  Malloc() errors (and other serious errors) result in program exiting
  */
+
+
+#ifndef _LINKED_LIST_H_
+#define _LINKED_LIST_H_
 
 #include <stdlib.h>
 #include "../mds/mds_errors.h"
@@ -29,3 +55,53 @@ typedef node* ll_cursor;
 struct linked_list* ll_init();
 void insert_node(struct linked_list, struct generic_data, uint);
 struct generic_data delete_node(struct linked_list, int); 
+
+/* Returns the number of elements in the list */
+int ll_size(struct linked_list *l);
+/* Returns whether the list is empty or not */
+int ll_isEmpty(struct linked_list *l);
+
+/* Adds a new element in the list at position i, shifting elements right to make room */
+void ll_insert(struct linked_list *l, struct generic_data d, int i);
+/* Insert d after the first occurence of s and return the index of s, or return -1 if s is not found */
+int ll_insert_after(struct linked_list *l, struct generic_data d, struct generic_data s);
+/* Insert d before the first occurence of s; return 1 on success or -1 if s is not found */
+int ll_insert_before(struct linked_list *l, struct generic_data d, struct generic_data s);
+/* Wrapper function to prepend d to the list (ie: insert on the first position) */
+void ll_insert_beginning(struct linked_list *l, struct generic_data d);
+/* Wrapper function to append d to the list */
+void ll_insert_end(struct linked_list *l, struct generic_data d);
+/* Append list a to list l 
+ * List a is NOT removed */
+void ll_append(struct linked_list *l, struct linked_list *a);
+
+
+/* Removes and returns the element at position i; shifts following
+ * elements to the left one position */
+struct generic_data ll_remove(struct linked_list* l, int i);
+
+/* Returns the element at position i in the list, without removing it */
+struct generic_data ll_get(struct linked_list* l, int i);
+/* Returns the element at position i and replaces it with d */
+struct generic_data ll_set(struct linked_list* l, struct generic_data d, int i);
+
+/* Returns a newly created and initialized linked list */
+struct linked_list* ll_init();
+/* Walks the list and applies function f to every element in order */
+void ll_walk(struct linked_list* l, void (*f)(struct generic_data));
+
+/* Removes all the elements in the list */
+void ll_clear(struct linked_list* l);
+/* Returns an array with all the elements of the list in the same order */
+struct generic_data* ll_toArray(struct linked_list* l);
+
+/* Search and sort methods
+ * Note: a function to compare two elements needs to be implemented */
+/* Returns the index of the first element equal to d, or -1 */
+int ll_indexOf(struct linked_list *l, struct generic_data d);
+/* Returns the index of the last element equal to d, or -1 */
+int ll_lastIndexOf(struct linked_list *l, struct generic_data d);
+/* Sort the list according to cmp */
+void ll_sort(struct linked_list *l);
+
+#endif
