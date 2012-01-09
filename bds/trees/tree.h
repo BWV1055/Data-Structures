5a6670561350192747becddecdff28271e9c4bb7
BWV1055 <zarzu.razvan@gmail.com>
2012-01-09 18:11:06 +0200

    Tree interface updated with pruning and grafting

--- bds/trees/tree.h
+++ bds/trees/tree.h
@@ -7,75 +7,92 @@
  * The problem with tree values is more subtle:
  * 	There can be multiple nodes with the same value and it's difficult to specify
  * 	a node based on the value
- * So tree methods work with nodes 
+ * So tree methods work with nodes, and sometimes with pointers to nodes 
  *
  * The update methods provided here are generic
- * For different types of trees/applications, additional update methods can be used
+ * For different types of trees/applications, additional update methods are available
  *
  */
 
+#ifndef _TREE_H_
+#define _TRRE_H_
+
 #include <stdio.h>
+#include "../mds/mds_data_types.h"
 
-#define TR_NODE_MAX_CHILDREN 8
-#define TR_MAX_NODES		 4096
-#define TR_MAX_DEPTH		 128
-#define MAX(a,b)			 (a)>(b)?(a):(b)
+#define TR_NODE_MAX_CHILDREN 		8
+#define TR_MAX_NODES		 		4096
+#define TR_MAX_DEPTH		 		128
 
-/* Path type
- * typedef unsigned char[TR_MAX_DEPTH] path; */
-
-struct generic_data {
-	int val;
+/* Path type */
+typedef uchar[TR_MAX_DEPTH] legs_t;
+/* Full path */
+struct fpath {
+	legs_t legs;
+	size_t len;
 };
 
-struct tree_node {
-	int nr_chd;
+struct tr_node {
+	int n_chd;
 	struct generic_data data;
-	struct tree_node *parent;
-	struct tree_node *children[TR_NODE_MAX_CHILDREN];
+	struct tr_node *parent;
+	struct tr_node **children;
 };
 
-struct tree_actual {
-	struct tree_node *root;
-};
-
-/* Used to keep statistics about a tree
- * */
 struct tree {
+	struct tr_node *root;
 	int size;
 	int depth;
-	struct tree_node root;
 };
+
+typedef struct tr_node* tr_cursor;
+
 /* Returns the number of nodes in the tree */
-int tr_size(struct tree *t);
+size_t tr_size(struct tree *t);
 /* Returns 1 if the tree has no elements, 0 otherwise */
-int tr_isEmpty(struct tree *t);
+uchar tr_isEmpty(struct tree *t);
 /* Returns a newly created and initialized tree
  * This is the way to use the other functions described here */
 struct tree* tr_init();
-
+/* Initialize tree with a value */
+struct tree* tr_init_data(struct generic_data data);
 /* Adds a new element in the tree at the position specified by path 
  * If there is already an element at this position, the function returns */
-void tr_insert(struct tree *t, struct generic_data d, int *path, int path_len);
-/* Removes the element found at the path path and returns it
- *  */
-struct generic_data tr_remove_path(struct tree *t, int *path, int path_len);
-/* Removes first occurence of data d in a breath first walk */
-char* tr_remove_data(struct tree *t, struct generic_data d);
-/* --- Accessor methods */
+void tr_insert(struct tree *t, struct generic_data data, struct fpath path);
+/* Replaces the element present at path and returns it */
+struct generic_data tr_replace(struct tree *t, struct fpath path, struct generic_data replace);
+/* Removes the element found at the fpath path, if the path exists 
+ * and returns it */
+struct generic_data tr_remove_path(struct tree *t, struct fpath path);
+/* Removes first occurence of data in a breath first traversal */
+char* tr_remove_data(struct tree *t, struct generic_data data);
 /* Returns the root */
-struct tree_node* tr_root(struct tree_node *t);
-/* Returns a list of the children */
-struct tree_node** tr_children(struct tree_node *t);
+tr_cursor tr_root(struct tree *t);
+/* Returns a list of the children of the selected node */
+tr_cursor* tr_children(tr_cursor cur);
 /* Returns the parent */
-struct generic_data tr_parent(struct tree_node *t);
-
-/* Walks the tree breath first and applies function f to every element */
-void tr_walk_bf(struct tree *t, void(*f)(struct tree_node));
-/* Walks the tree depth first and applies function f to every element */
-void tr_walk_df(struct tree *t, void(*f)(struct generic_data));
+tr_cursor tr_parent(tr_cursor cur);
+/* Return 1 if node if leaf, 0 otherwise */
+uchar tr_leaf(struct tree *t, struct tr_node node);
 /* Returns an array with all siblings */
-struct generic_data* tr_siblings(struct tree *t, int *path, int path_len);
+struct generic_data* tr_siblings(struct tree *t, struct fpath path);
 
-/* Returns the height of a tree in O(n) */
-int tr_height(struct tree*t, struct tree_node *n);
+/* Traverses the tree breath first and applies function f to every element 
+ * f can do many more things if it works with the actual node, and not with the data in it */
+void tr_traverse_bf(struct tree *t, void (*f)(struct tr_node));
+/* Traverses the tree depth first and applies function f to every element */
+void tr_traverse_df(struct tree *t, void (*f)(struct tr_node));
+
+/* Returns the height of a sub-tree in O(n) */
+uint tr_height(struct tree *t, tr_cursor cur);
+/* Returns the depth of a sub-tree */
+uint tr_depth(struct tree *t, tr_cursor cur);
+/* Takes a path and returns a cursor */
+tr_cursor tr_path_to_cur(struct tree *t, struct fpath path);
+/* Removes a subtree pointed by cur, returns the parent of cur */
+tr_cursor tr_remove_subtr(struct tree *t, tr_cursor cur);
+/* Adds a subtree to_add on the next available position as a child of cur 
+ * Returns NULL if the operation failed, or to_add on success */
+tr_cursor tr_add_subtr(struct tree *t, tr_cursor to_add, tr_cursor cur);
+
+#endif
