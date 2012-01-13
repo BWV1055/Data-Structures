f0fb7d4bc59998b2c47e1e08b3735cd54fa32648
BWV1055 <zarzu.razvan@gmail.com>
2012-01-13 14:39:55 +0200

    Adjacency list add, remove and walk methods

--- graphs/linked_list.cpp
+++ graphs/linked_list.cpp
@@ -4,6 +4,8 @@
  *
  */
 
+using namespace std;
+
 /* key-value type entries 
  * The key is currently not used, the sorting is done on value */
 class Entry
@@ -11,29 +13,79 @@ class Entry
 	int value;
 	std::string key;
 public:
-	Entry(int value, std::string key);
+	Entry(int nValue, std::string nKey) : value(nValue), key(nKey) {}
 	int getValue();
 	std::string getKey();
 }
 
+
+
 class AdjLLEntry
 {
+public:
 	Vertex* adjacent;
-	AdjLLEntry *next;
+	AdjLLEntry* next;
 
 public:
-	void AdjLLEntry(Entry data, LLEntry *next);
-	Entry getData();
+	AdjLLEntry(Vertex* nAdjacent, AdjLLEntry* nNext) : adjacent(nAdjacent), next(nNext) {}
+	setAdjacent(Vertex* adj);
+	setNext
+	Vertex* getAdjacent();
 	AdjLLEntry* getNext();
 };
 
+Vertex* AdjLLEntry::getAdjacent() {
+	return this->adjacent;
+}
+
+AdjLLEntry* getNext() {
+	return this->next;
+}
+
 class AdjLinkedList
 {
-	int size;
-	AdjLLEntry list[100];
-
 public:
-	void add(AdjLLEntry adj);
+	int size;
+	AdjLLEntry* root;
+	AdjLLEntry* tail;
+public:
+	AdjLinkedList(int nSize = 0) : size(nSize), root(NULL) {}
+	void setRoot(AdjLLEntry* nRoot);
+	void add(Vertex* adj);
 	AdjLLEntry remove();
 	void walk((void*)(*f)(AdjLLEntry));
 };
+
+void AdjLinkedList::add(Vertex* adj) {
+	AdjLLEntry newAdj (adj, NULL);
+	if(!this->size) {
+		newAdj->setNext(this->root);
+		this->setRoot(&newAdj);
+		return;
+	}
+	this->tail->next = &newAdj;
+	this->tail = &newAdj;
+}
+
+AdjLLEntry AdjLinkedList::remove() {
+	AdjLLEntry* behind, it = this->root;
+	while(it!=tail) {
+		behind = it;
+		it = it->next;
+	}
+	behind->next = NULL;
+	this->tail = behind;
+	delete it;
+}
+/* If f returns NULL, walk() returns the position and stops there */
+int AdjLinkedList::walk((void*)(*f)(AdjLLEntry)) {
+	AdjLLEntry* it = this->root;
+	int pos = 0;
+	while(it) {
+		if(!f(*it))
+			return pos;
+		it = it->next;
+		pos++;
+	}
+	return pos;
+}
