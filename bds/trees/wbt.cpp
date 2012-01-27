5bf000236ab74d512ae69259c2883abd20118d1f
BWV1055 <zarzu.razvan@gmail.com>
2012-01-27 17:09:09 +0200

    Double left/right rotation

--- bds/trees/wbt.cpp
+++ bds/trees/wbt.cpp
@@ -5,5 +5,62 @@
  *	This implementation uses delta = %delta% and gamma = %gamma%
  */
 
-class WBT {
+#include "BinarySearchTree.cpp"
 
+class WBT_Node: public BSTNode {
+	int size;
+public: 
+	WBT_Node(int size, WBT_Node* left, WBT_Node* right): size(size), left(left), right(right) {}
+	int getSize() { return this->size; }
+	int setSize(int size) { this->size = size; }
+}
+
+class WBT {
+	WBT_Node* root;
+public:
+	WBT(): root(NULL) {}
+	void insert(K key, V value) {
+		this->base_add(key, value);
+	}
+}	
+
+// Double rotation
+void rotate_d_left(BSTNode* a) {
+	BSTNode* b = a->right;
+	BSTNode* c = b->left;
+	c->parent = a->parent;
+	if(a->parent) {
+		if(a->parent->left==a)
+			a->parent->left = c;
+		else
+			a->parent->right = c;
+	}
+	a->right = c->left;
+	a->right->parent = a;
+	b->left = c->right;
+	b->left->parent = b;
+	c->left = a;
+	a->parent = c;
+	c->right = b;
+	b->parent = c;
+}
+
+void rotate_d_right(BSTNode* a) {
+	BSTNode* b = a->left;
+	BSTNode* c = b->right;
+	c->parent = a->parent;
+	if(a->parent) {
+		if(a->parent->left==a)
+			a->parent->left = c;
+		else
+			a->parent->right = c;
+	}
+	a->left = c->right;
+	a->left->parent = a;
+	b->right = c->left;
+	b->right->parent = b;
+	c->right = a;
+	a->parent = c;
+	c->left = b;
+	b->parent = c;
+}
