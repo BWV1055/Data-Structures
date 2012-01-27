ca59930c9645da4774727f921ed8395bf385fef6
BWV1055 <zarzu.razvan@gmail.com>
2012-01-27 14:58:05 +0200

    Weigth balanced (BB) trees

---
+++ bds/trees/wbt.cpp
@@ -0,0 +1,9 @@
+/*
+ *	Weight balanced trees
+ *		Original tree used irrational parameters, in Adams tree a bug was found
+ *		See http://hagi.is.s.u-tokyo.ac.jp/~yh/bst.pdf
+ *	This implementation uses delta = %delta% and gamma = %gamma%
+ */
+
+class WBT {
+
