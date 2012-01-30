ecfe5f6c16938ba912d765ca91b895a31bec64ed
BWV1055 <zarzu.razvan@gmail.com>
2012-01-30 11:44:44 +0200

    Swap macro as in GCC

--- algorithms/sort/quick_sort.h
+++ algorithms/sort/quick_sort.h
@@ -6,3 +6,16 @@
 #include "../../mds/mds_data_types.h"
 
 void quick_sort(int len; struct generic_data ar[len], int len);
+
+#define SWAP(a, b, size) \
+	do { \
+		register size_t __size = (size); \
+		register char* __a = (a), * __b = (b); \
+		do { \
+			char __temp = *__a; \
+		   	*__a++ = *__b; \
+			*__b++ = __temp; \
+		} while(--__size>0); \
+	}while(0) 
+
+
